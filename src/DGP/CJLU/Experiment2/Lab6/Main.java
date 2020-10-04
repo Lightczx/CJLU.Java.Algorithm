bbd60eac2d1a9b584b3bdded24ea2e8a8794436f
Lightczx <1686188646@qq.com>
2020-10-05 00:03:28 +0800

    SingleLinkedList

--- src/DGP/CJLU/Experiment2/Lab6/Main.java
+++ src/DGP/CJLU/Experiment2/Lab6/Main.java
@@ -7,16 +7,21 @@ import DGP.CJLU.Utils.Implementation.Rand;
  * Write a nonrecursive procedure to reverse a singly linked list in O(N) time.
  */
 public class Main {
+
     public static void main(String[] args) {
+        SingleLinkedList singleLinkedList = InitializeSingleLinkedList();
+        System.out.println(singleLinkedList.toString());
         new ExecuteTime().run(() -> {
-            InitializeSingleLinkedList().reverse();
+
+            singleLinkedList.reverse();
         });
+        System.out.println(singleLinkedList.toString());
     }
 
     private static SingleLinkedList<Integer> InitializeSingleLinkedList() {
         SingleLinkedList<Integer> list = new SingleLinkedList<>();
         for (int j = 1; j <= 10; j++)
-            list.add(Rand.randInt(1, 10));
+            list.addFirst(Rand.randInt(1, 10));
         return list;
     }
 
--- src/DGP/CJLU/Experiment2/Lab6/SingleLinkedList.java
+++ src/DGP/CJLU/Experiment2/Lab6/SingleLinkedList.java
@@ -2,6 +2,7 @@ package DGP.CJLU.Experiment2.Lab6;
 
 /**
  * single linked list custom implementation
+ *
  * @param <E> the stored value type
  */
 public class SingleLinkedList<E> {
@@ -14,12 +15,13 @@ public class SingleLinkedList<E> {
             this.next = next;
         }
 
-        public Node(E e) {
-            this(e, null);
-        }
     }
 
-    private Node head;
+    /**
+     * dummy head
+     */
+    private Node dummyHead;
+    private Node dummyTail;
     private int size;
 
     public int getSize() {
@@ -27,127 +29,49 @@ public class SingleLinkedList<E> {
     }
 
     public SingleLinkedList() {
-        this.head = null;
+        this.dummyTail=new Node(null,null);
+        this.dummyHead = new Node(null, dummyTail);
         this.size = 0;
     }
 
+    /**
+     * O(1)
+     *
+     * @param e
+     */
     public void addFirst(E e) {
-        this.head = new Node(e, head);
+        Node node = new Node(e, dummyHead.next);
+        this.dummyHead.next = node;
         this.size++;
-    }
-
-    /**
-     * 向链表尾部插入元素
-     */
-    public void add(E e) {
-        this.add(e, this.size);
-    }
-
-    /**
-     * 向链表中间插入元素
-     */
-    public void add(E e, int index) {
-        if (index < 0 || index > size)
-            throw new IllegalArgumentException();
-        if (index == 0)
-            this.addFirst(e);
-
-        Node pre = this.head;
-        //找到要插入节点的前一个节点
-        for (int i = 0; i < index - 1; i++)
-            pre = pre.next;
-
-        Node node = new Node(e);
-        node.next = pre.next;
-        pre.next = node;
-        this.size++;
-    }
-
-    public E removeFirst() {
-        if (this.head == null) {
-            //no element to remove
-            return null;
-        }
-        Node delNode = this.head;
-        this.head = this.head.next;
-        delNode.next = null;
-        this.size--;
-        return delNode.e;
-    }
-
-    public E removeLast() {
-        if (this.head == null) {
-            return null;
-        }
-        //只有一个元素
-        if (this.size == 1) {
-            return this.removeFirst();
-        }
-        Node cur = this.head;    //记录当前结点
-        Node pre = this.head;    //记录要删除结点的前一个结点
-        while (cur.next != null) {
-            pre = cur;
-            cur = cur.next;
-        }
-        pre.next = cur.next;
-        this.size--;
-        return cur.e;
-    }
-
-    public void remove(E e) {
-        Node dummy = new Node(e, this.head);
-        Node cur = dummy;
-
-        while (cur.next != null) {
-            if (cur.next.e.equals(e)) {
-                cur.next = cur.next.next;
-                this.size--;
-            } else cur = cur.next;
-        }
-
-        this.head = dummy.next;
-    }
-
-    /**
-     * @param e:插入在t元素的位置
-     * @param destination：要插入的元素
-     */
-    public void insert(E e, E destination) {
-        Node dummy = new Node(null, this.head);
-        Node dNode = new Node(destination);
-
-        Node cur = dummy;
-        while (cur.next != null) {
-            if (cur.next.e.equals(e)) {
-                dNode.next = cur.next;
-                cur.next = dNode;
-                this.size++;
-                break;
-            } else cur = cur.next;
-        }
-
-        this.head = dummy.next;
-    }
-
-    public boolean contains(E t) {
-        Node cur = this.head;
-        while (cur != null) {
-            if (cur.e.equals(t)) {
-                return true;
-            } else cur = cur.next;
-        }
-        return false;
     }
 
     public void reverse() {
-        Node head = this.head;
         Node pre = null;
-        Node next = null;
-        while (head != null) {
-            next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
+        Node cur = dummyHead;
+        Node next;
+        while (cur!=null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
+        //swap head and tail
+        Node tmp=dummyHead;
+        dummyHead=dummyTail;
+        dummyTail=tmp;
+    }
+
+    @Override
+    public String toString() {
+        Node cur = dummyHead.next;
+        StringBuilder sb = new StringBuilder();
+        sb.append("head -> ");
+        while (!cur.equals(dummyTail)) {
+                sb.append(cur.e);
+            sb.append(" -> ");
+            cur = cur.next;
+        }
+        sb.append("tail");
+        return sb.toString();
     }
 }
