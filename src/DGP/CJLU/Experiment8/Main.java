71e16e9419e8417dae3710b63387354accd14925
Lightczx <1686188646@qq.com>
2020-12-14 10:08:08 +0800

    up

---
+++ src/DGP/CJLU/Experiment8/Main.java
@@ -0,0 +1,14 @@
+package DGP.CJLU.Experiment8;
+
+/**
+ * 1.	Using the graph as figure 7.1,
+ * a)	Write out the results of topological sorting
+ * b)	Write a program to perform a topological sort according to the pseudocode showed in Figure 9.7 in the textbook.
+ * 2.	Write a program to implement Dijkstra’s algorithm to solve the single-source shortest path problem(the source is A) of the graph in Figure 7.2, and write out the results.
+ * 3.	Write a program to implement Prim’s algorithm to get a minimum spanning tree from the graph in Figure 7.3, and write out the result.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
